672a0595915976f30520ff95d5c7e5deeedd34ec
Dimitre <dimitre@users.noreply.github.com>
2020-09-03 19:04:30 -0300

    ofxMiaw

---
+++ src/ofxMiaw.h
@@ -0,0 +1,34 @@
+//
+
+#define USESVG 1
+#ifdef USESVG
+#include "ofxSvg.h"
+#endif
+
+class ofxScenes : public ofBaseApp {
+public:
+
+	
+#define USESVG 1
+	ofxMicroUI * _u = NULL;
+	ofFbo * fbo = NULL;
+	ofxMicroUI * ui = NULL;
+	ofxMicroUI * uiC = NULL;
+	string & scene;
+	
+	ofxScenes(ofFbo * _f, ofxMicroUI * _ui, ofxMicroUI * _uiC, string & s) :
+	fbo(_f), ui(_ui), uiC(_uiC), scene(s) {
+//		scene = ui->pString["scene"];
+	}
+	
+	void setup() {
+		setupScenesNova();
+	}
+	
+	void draw() {
+		drawScenesNova();
+	}
+
+#include "scenes.h"
+	
+};
